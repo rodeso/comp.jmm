c7c920c3ab5c6ededd1c751866af5680101dcb38
Rafael Campeão <up202207553@edu.fe.up.pt>
2025-05-03 23:17:15 +0100

    prevention fix for constant folding

--- src/main/pt/up/fe/comp2025/optimization/ConstantFolding.java
+++ src/main/pt/up/fe/comp2025/optimization/ConstantFolding.java
@@ -50,7 +50,11 @@ public class ConstantFolding extends PreorderJmmVisitor<Void, Void> {
 
             this.setHasModified(true);
 
-            expr.replace(newChild);
+            //expr.replace(newChild);
+            JmmNode parent = expr.getParent();
+            int childIndex = expr.getIndexOfSelf();
+            parent.removeChild(childIndex);
+            parent.add(newChild,childIndex);
 
             return null;
 
